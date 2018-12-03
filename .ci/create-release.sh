7cd875fdd1b31a0b519b490f734d4c4ed6640b52
Housni Yakoob <housni.yakoob@gmail.com>
2018-12-03 15:49:37 +0300

    Bash 3.x friendly

--- .ci/create-release.sh
+++ .ci/create-release.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+# NOTE: Bash on Travis's OSX is at version 3.x so don't go too crazy with Bash.
+
 set -x
 set -euo pipefail
 IFS=$'\n\t'
 
 
 # TRAVIS_BRANCH="master"
-# TRAVIS_COMMIT_MESSAGE="merge foo bar into housni/g"
+# TRAVIS_COMMIT_MESSAGE="merge foo bar into myname/gen 232"
 
 set +eu
 
 
 
-if [[ "$TRAVIS_BRANCH" != "master" ]] && [[ -z "$TRAVIS_TAG" ]]; then
+if [[ "${TRAVIS_BRANCH:-}" != "master" ]] && [[ -z "${TRAVIS_TAG:-}" ]]; then
   echo "You are not merging into master nor is a tag present."
   exit 1
 fi
@@ -22,12 +24,12 @@ declare RELEASE_FILENAME
 
 # On merge to 'master', generate [jira_ticket_id].app.tgz release.
 # Ticket name can safely be assumed based on the regexp /(gen[^0-9]+[0-9]+)/i.
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "${TRAVIS_BRANCH:-}" == "master" ]]; then
   shopt -s nocasematch
   pattern="(gen[^0-9]+[0-9]+)"
-  if [[ "$TRAVIS_COMMIT_MESSAGE" =~ $pattern ]];then
+  if [[ "${TRAVIS_COMMIT_MESSAGE:-}" =~ $pattern ]];then
     declare jira_ticket_id
-    jira_ticket_id="${BASH_REMATCH[1]^^}" # convert to upper case
+    jira_ticket_id=$(echo "${BASH_REMATCH[1]}" | tr '[:lower:]' '[:upper:]')
     export RELEASE_FILENAME=${jira_ticket_id// /-} # replace spaces with hyphens
   else
     echo "ERROR: Unable to get JIRA branch name from the merge to master."
@@ -35,16 +37,8 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   fi
 fi
 
-
-
-
-echo "RELEASE_FILENAME: $RELEASE_FILENAME"
-
-
-
-
 # On tag, generate [GIT_TAG].tgz release
-if [[ -n "$TRAVIS_TAG" ]]; then
+if [[ -n "${TRAVIS_TAG:-}" ]]; then
   export RELEASE_FILENAME="$TRAVIS_TAG"
 fi
 
