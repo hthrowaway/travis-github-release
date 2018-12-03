d289376e9eeea6b19d74bfa2b373f61721f8b7db
Housni Yakoob <housni.yakoob@gmail.com>
2018-12-03 15:55:23 +0300

    Workaround

--- .ci/create-release.sh
+++ .ci/create-release.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 
 
 # TRAVIS_BRANCH="master"
-# TRAVIS_COMMIT_MESSAGE="merge foo bar into myname/gen 232"
+# TRAVIS_COMMIT_MESSAGE="merge foo bar into myname/ge32"
 
 set +eu
 
@@ -26,7 +26,7 @@ declare RELEASE_FILENAME
 # Ticket name can safely be assumed based on the regexp /(gen[^0-9]+[0-9]+)/i.
 if [[ "${TRAVIS_BRANCH:-}" == "master" ]]; then
   shopt -s nocasematch
-  pattern="(gen[^0-9]+[0-9]+)"
+  pattern="(gen[^0-9]+[0-9]+[^0-9]*)"
   if [[ "${TRAVIS_COMMIT_MESSAGE:-}" =~ $pattern ]];then
     declare jira_ticket_id
     jira_ticket_id=$(echo "${BASH_REMATCH[1]}" | tr '[:lower:]' '[:upper:]')
